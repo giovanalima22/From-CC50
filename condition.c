bf03013a2dfec541fa6f45cda3d89cd57e1bc70c
Gio <112208073+giovanalima22@users.noreply.github.com>
2022-08-27 19:27:04 -0300

    Update condition.c

--- condition.c
+++ condition.c
@@ -21,3 +21,26 @@ int main(void)
      printf("x é igual a y\n");
     }
  }
+
+
+------------------------------------------
+ //Outro Exemplo 
+ 
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+  //pergunta ao usuario
+    char c = get_char("Do you agree? ");
+  
+ //condição se concorda ou se não concorda
+    if (c == 'y' || 'Y')
+    {
+        printf("Agreed.\n");
+    }
+    else if (c == 'n' || 'N')
+    {
+        printf("Not agreed.\n");
+    }
+}
