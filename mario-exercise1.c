b919acb2bd1f001c4c208d7bbc5b36f67e1f7071
Gio <112208073+giovanalima22@users.noreply.github.com>
2022-08-27 21:13:54 -0300

    Create mario-exercise1.c

---
+++ mario-exercise1.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int h;
+    do
+    {
+        h = get_int("Altura: ");
+    } //condição para altura da piramide
+        while(h < 1 || h > 8);
+
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < h; j++)
+            {
+                if (i + j < h - 1)
+                {
+                    printf(" ");
+                }
+                else
+                {
+                    printf("#");
+                }
+            }
+            printf("\n");
+        }
+}
