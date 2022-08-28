7631277d4f5d840f1ad58743d5c3b8843a328e92
Gio <112208073+giovanalima22@users.noreply.github.com>
2022-08-28 10:30:44 -0300

    Update mario-exercise1.c
    CS50 Problem Set 1 (PSet1) Mario Walkthrough

--- mario-exercise1.c
+++ mario-exercise1.c
@@ -3,26 +3,25 @@
 
 int main(void)
 {
-    int h;
+    int altura;
     do
     {
-        h = get_int("Altura: ");
-    } //condição para altura da piramide
-        while(h < 1 || h > 8);
-
-        for (int i = 0; i < h; i++)
-        {
-            for (int j = 0; j < h; j++)
-            {
-                if (i + j < h - 1)
-                {
-                    printf(" ");
-                }
-                else
-                {
-                    printf("#");
-                }
+       altura = get_int("Altura: ");
+    }//enquanto a altura for menor que 1 ou maior que 8 continue perguntando "Altura: "
+    while ( altura < 1 || altura > 8);
+    //para linha começa em 0; linha menor que altura; incrementa +1 linha
+    for (int linha = 0; linha < altura; linha++)
+    {// para coluna começa em 0; coluna menor que altura; incrementa +1 coluna
+        for (int coluna = 0; coluna < altura; coluna++)
+          {// se a linha + a coluna for menor que a altura da piramide - 1
+              if (linha + coluna < altura - 1)
+                printf(" ");//printa um espaço em branco
+            else {//senao printa uma hash
+                printf("#");
             }
-            printf("\n");
-        }
+
+          }
+    printf("\n");
+    }
+
 }
