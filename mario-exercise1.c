23b5a3c0440383055c6151b24173cbb69ef28a01
Gio <112208073+giovanalima22@users.noreply.github.com>
2022-08-28 11:06:21 -0300

    Update mario-exercise1.c

--- mario-exercise1.c
+++ mario-exercise1.c
@@ -3,25 +3,26 @@
 
 int main(void)
 {
-    int altura;
+    int altura, linha, coluna, espaco;
+
     do
     {
        altura = get_int("Altura: ");
     }//enquanto a altura for menor que 1 ou maior que 8 continue perguntando "Altura: "
     while ( altura < 1 || altura > 8);
-    //para linha começa em 0; linha menor que altura; incrementa +1 linha
-    for (int linha = 0; linha < altura; linha++)
-    {// para coluna começa em 0; coluna menor que altura; incrementa +1 coluna
-        for (int coluna = 0; coluna < altura; coluna++)
-          {// se a linha + a coluna for menor que a altura da piramide - 1
-              if (linha + coluna < altura - 1)
-                printf(" ");//printa um espaço em branco
-            else {//senao printa uma hash
-                printf("#");
-            }
+    
+    for (linha = 0; linha < altura; linha++)
+    {
+        for (espaco = 0; espaco < altura - linha - 1; espaco++)
+        {
+            printf(" ");//printa um espaço em branco
+        }
 
-          }
-    printf("\n");
+        for (coluna = 0; coluna <= linha; coluna++)
+        {//senao printa uma hash
+            printf("#");
+        }
+        printf("\n");
     }
 
 }
